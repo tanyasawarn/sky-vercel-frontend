8445083636078c264f1caf6463918553346782bc
Tanya <tanya@Tanyas-MacBook-Air.local>
2026-04-18 14:04:46 +0530

    remove static export

--- next.config.js
+++ next.config.js
@@ -1,26 +1,13 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "cdn.jsdelivr.net",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true, // REQUIRED for static export
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
